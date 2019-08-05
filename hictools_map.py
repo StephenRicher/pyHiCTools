22f8a90c55051319d7ca3bea1382a40be9738b5c
Stephen Richer <sr467@bath.ac.uk>
2019-08-05 23:54:17 +0100

    Close pipe to parent process as new pipe is created.

--- hictools_map.py
+++ hictools_map.py
@@ -17,7 +17,8 @@ def description():
         
     return __doc__
 
-def map(infiles, output, index, threads, samtools, bowtie2, bowtie2_log, sam_out):
+def map(infiles, output, index, threads, 
+        samtools, bowtie2, bowtie2_log, sam_out):
     
     ''' Map R1 and R2 of HiC paired-end reads seperately. '''
     
@@ -48,13 +49,16 @@ def map(infiles, output, index, threads, samtools, bowtie2, bowtie2_log, sam_out
                     p2 = stack.enter_context(
                         Popen(cmd2, stdin = p1.stdout, 
                             stdout = PIPE, stderr = tmp))
+                    p1.stdout.close()
                     p3 = stack.enter_context(
                         Popen(cmd3, stdin = p2.stdout, stderr = tmp))
+                    p2.stdout.close()
                     
                     exit_codes = [p.wait() for p in [p1, p2, p3]]
                     log.debug(f'Exit_codes for p1, p2, p3: {exit_codes}.')
                     if not all(ec is 0 for ec in exit_codes):
-                        log.error('A sub-process returned a non-zero exit code.')
+                        log.error(
+                            'A sub-process returned a non-zero exit code.')
                         
             cmd4 = [f'{samtools}', 'merge', '-n', '-@', f'{threads}', 
                 '-', 'R1.sorted.tmp.bam', 'R2.sorted.tmp.bam']
@@ -71,12 +75,16 @@ def map(infiles, output, index, threads, samtools, bowtie2, bowtie2_log, sam_out
                     Popen(cmd4, stdout = PIPE, stderr = tmp))
                 p5 = stack.enter_context(
                     Popen(cmd5, stdin = p4.stdout, stdout = PIPE, stderr = tmp))
+                p4.stdout.close()
                 p6 = stack.enter_context(
                     Popen(cmd6, stdin = p5.stdout, stdout = PIPE, stderr = tmp))
+                p5.stdout.close()
                 p7 = stack.enter_context(
                     Popen(cmd7, stdin = p6.stdout, stdout = PIPE, stderr = tmp))
+                p6.stdout.close()
                 p8 = stack.enter_context(
                     Popen(cmd8, stdin = p7.stdout, stderr = tmp))
+                p7.stdout.close()
 
                 exit_codes = [p.wait() for p in [p4, p5, p6, p7]]
                 
