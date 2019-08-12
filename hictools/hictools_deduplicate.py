a9b9a89c9b4077ebb7b6df1e7dff45448535d199
Stephen Richer <sr467@bath.ac.uk>
2019-08-12 09:15:48 +0100

    Remove fixmate and piped commands output to SAM.

--- hictools/hictools_deduplicate.py
+++ hictools/hictools_deduplicate.py
@@ -26,16 +26,12 @@ def deduplicate(infile, output, threads, samtools, sam_out):
     out_format = 'S' if sam_out else 'b'
     stdin = sys.stdin if infile == '-' else None
 
-    cmd1 = [f'{samtools}', 'sort', '-l', '0', '-m', '1G', 
+    cmd1 = [f'{samtools}', 'sort', '-O', 'SAM', '-m', '1G', 
         '-@', f'{threads}', f'{infile}']
-    cmd2 = [f'{samtools}', 'markdup', '-sr', 
+    cmd2 = [f'{samtools}', 'markdup', '-sr', '-O', 'SAM',
         '-@', f'{threads}', '-', '-']
     cmd3 = [f'{samtools}', 'sort', '-l', '0', '-n', '-m', '1G', 
-        '-@', f'{threads}', '-']
-    cmd4 = [f'{samtools}', 'fixmate', '-p', 
-        '-@', f'{threads}', '-', '-']
-    cmd5 = [f'{samtools}', 'view', f'-{out_format}h', '-f', '1', 
-        '-@', f'{threads}','-o', f'{output}']
+        '-@', f'{threads}', '-o', f'{output}', '-']
         
     with ExitStack() as stack:
         try:
@@ -46,14 +42,8 @@ def deduplicate(infile, output, threads, samtools, sam_out):
                 Popen(cmd2, stdin = p1.stdout, stdout = PIPE))
             p1.stdout.close()
             p3 = stack.enter_context(
-                Popen(cmd3, stdin = p2.stdout, stdout = PIPE, stderr = tmp))
+                Popen(cmd3, stdin = p2.stdout, stderr = tmp))
             p2.stdout.close()
-            p4 = stack.enter_context(
-                Popen(cmd4, stdin = p3.stdout, stdout = PIPE, stderr = tmp))
-            p3.stdout.close()
-            p5 = stack.enter_context(
-                Popen(cmd5, stdin = p4.stdout, stderr = tmp))
-            p4.stdout.close()
             
             exit_codes = [p.wait() for p in [p1, p2, p3]]
             log.debug(f'Exit_codes for p1, p2, p3: {exit_codes}.')
